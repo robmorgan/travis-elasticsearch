76ee56e8836a0f8193ac08c0bd26906c3b43ab88
Juzer Ali <er.juzerali@gmail.com>
2012-12-29 15:34:16 +0530

    Changed to elastic search

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
-travis-solr.sh
+travis-elasticsearch.sh
 ==============
 
-Get a Solr instance running with a one-liner and use it in your tests.
+Get a elasticsearch instance running with a one-liner and use it in your tests.
 
 
 Usage
@@ -9,38 +9,24 @@ Usage
 
 ::
 
-  curl https://raw.github.com/moliware/travis-solr/master/travis-solr.sh | SOLR_VERSION=3.6.1 SOLR_CONFS=schema.xml solrconfig.xml SOLR_DOCS=custom_docs.json bash
+  curl https://raw.github.com/juzerali/travis-elasticsearch/master/travis-elasticsearch.sh | ELASTICSEARCH_VERSION=0.20.2 bash
 
-SOLR_VERSION:
+ELASTICSEARCH_VERSION:
 .............
 
 You have to specify one of these versions:
 
-- 3.5.0
-- 3.6.0
-- 3.6.1
-- 4.0.0
-
-SOLR_CONFS:
-...........
-
-If you need to use some custom configuration you can specify one or more files 
-in this variable and the script will copy it in the solr conf folder.
-
-Don't use it if you need the default solr settings.
-
-SOLR_DOCS:
-..........
-
-By default the script will index exampledocs/books.json documents. You can point
-this variable to a json file that contains your custom documents.
+- 0.20.2
+- 0.20.1
+- 0.20.0
+- 0.19.11
 
 
 Travis-ci
 ---------
 
-Edit your .travis.yml and use travis-solr as a *before_script* script. 
-For example if you want to use solr 3.6.1 with the default settings you can add this
+Edit your .travis.yml and use travis-elasticsearch as a *before_script* script. 
+For example if you want to use elasticsearch 3.6.1 with the default settings you can add this
 line to your .travis.yml: ::
 
-  before_script: curl https://raw.github.com/moliware/travis-solr/master/travis-solr.sh | SOLR_VERSION=3.6.1 bash
+  before_script: curl https://raw.github.com/juzerali/travis-elasticsearch/master/travis-elasticsearch.sh | ELASTICSEARCH_VERSION=0.20.2 bash
---
+++ travis-elasticsearch.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+download() {
+    echo "Downloading elasticsearch from $1..."
+    curl -s $1 | tar xz
+    echo "Downloaded"
+}
+
+is_elasticsearch_up(){
+    http_code=`echo $(curl -s -o /dev/null -w "%{http_code}" "http://localhost:9200")`
+    return `test $http_code = "200"`
+}
+
+wait_for_elasticsearch(){
+    while ! is_elasticsearch_up; do
+        sleep 3
+    done
+}
+
+run() {
+    echo "Starting elasticsearch ..."
+    cd $1/bin
+    if [ $DEBUG ]
+    then
+        elasticsearch &
+    else
+        elasticsearch  > /dev/null 2>&1 &
+    fi
+    wait_for_elasticsearch
+    cd ../../
+    echo "Started"
+}
+
+post_some_documents() {
+    #TODO
+}
+
+
+download_and_run() {
+    case $1 in
+        0.20.2)
+            url="http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.2.tar.gz"
+            dir_name="elasticsearch-0.20.2"
+            ;;
+        0.20.1)
+            url="http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.2.tar.gz"
+            dir_name="elasticsearch-0.20.1"
+            ;;
+        0.20.0)
+            url="http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.2.tar.gz"
+            dir_name="elasticsearch-0.20.0"
+            ;;
+        0.19.11)
+            url="http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.2.tar.gz"
+            dir_name="elasticsearch-0.19.11"
+            ;;
+    esac
+
+    download $url
+
+    # copies custom configurations
+
+    #for file in $ELASTICSEARCH_CONFS
+    #do
+    #    if [ -f $file ]
+    #    then
+    #        cp $file $dir_name/example/elasticsearch/conf/
+    #        echo "Copied $file into elasticsearch conf directory."
+    #    fi
+    #done
+
+    # Run elasticsearch
+    run $dir_name
+
+    # Post documents
+    #if [ -z "$ELASTICSEARCH_DOCS" ]
+    #then
+    #    echo "Indexing some default documents"
+    #    post_some_documents $dir_name $dir_name/example/exampledocs/books.json
+    #else
+    #    echo "Indexing $ELASTICSEARCH_DOCS"
+    #    post_some_documents $dir_name $ELASTICSEARCH_DOCS
+    #fi
+}
+
+check_version() {
+    case $1 in
+        0.20.2|0.20.1|0.20.0|0.19.11);;
+        *)
+            echo "Sorry, $1 is not supported or not valid version."
+            exit 1
+            ;;
+    esac
+}
+
+
+check_version $ELASTICSEARCH_VERSION
+download_and_run $ELASTICSEARCH_VERSION
